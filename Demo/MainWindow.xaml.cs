dd1d693ca84eea6569dabb09f7d973e3f63b34fe
yinweikang <Jasper_yin@xnebula.com>
2024-11-07 16:26:56 +0800

    布局

--- Demo/MainWindow.xaml.cs
+++ Demo/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Demo
 {
@@ -16,9 +8,32 @@ namespace Demo
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool isDragging = false;
+        private Point dragStartPoint = new Point();
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void DraggableTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            isDragging = true;
+            dragStartPoint = e.GetPosition(draggableTextBlock);
+            //draggableTextBlock.CaptureMouse(); // 捕获鼠标，确保在拖拽过程中接收所有鼠标事件
+        }
+
+        private void centerContent_MouseEnter(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Border_MouseEnter(object sender, MouseEventArgs e)
+        {
+            draggableTextBlock.ReleaseMouseCapture();
+
+            if (isDragging)
+            {
+                var aaa = "";
+            }
         }
     }
 }
